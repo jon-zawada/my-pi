c43d9dad52241c7be20811a33b1c0ded55113783
Jon Zawada <jonathan_zawada@yahoo.com>
2025-05-24 17:15:26 -0700

    bug: button opens new tab to navigate to /health

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/health"
-            target="_blank"
             rel="noopener noreferrer"
           >
             <Image
